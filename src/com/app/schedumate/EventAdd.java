1ef8ad0739d3201e673f6813fb721eb28656dc71
Aritra Samanta <asamanta@purdue.edu>
2014-10-14 16:28:25 -0400

    EventAddClass
    EventAddClass created and implemented. Testing going on.

--- src/com/app/schedumate/EventAdd.java
+++ src/com/app/schedumate/EventAdd.java
@@ -9,9 +9,14 @@
 
 package com.app.schedumate;
 
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
+import android.content.ContentValues;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class EventAdd {
 	
@@ -23,11 +28,79 @@ public class EventAdd {
 			sdc_helper.COLUMN_COURSE_TIME,
 			sdc_helper.COLUMN_COURSE_LOCATION
 		};
-			
 	
+	public EventAdd(Context context){
+		sdc_helper = new SchedumateDatabaseClass(context);
+	}
 	
-	public EventAdd(Context context)
-	{
-		sdc_helper = new SchedumateDatabaseClass();
+	public void open() throws SQLException {
+		db = sdc_helper.getWritableDatabase();
+	}
+	
+	public void close() {
+		sdc_helper.close();
+	}
+	
+	public CourseDetails createCourse(long course_number, String course_name, String 
+			location, String time) {
+		ContentValues values = new ContentValues();
+		CourseDetails cd = new CourseDetails();
+		
+		cd.setCourseNumber(course_number);
+		cd.setCourseName(course_name);
+		cd.setLocation(location);
+		cd.setTime(time);
+		
+		values.put(sdc_helper.COLUMN_COURSE_NAME, cd.getCourseName());
+		values.put(sdc_helper.COLUMN_COURSE_NUMBER, cd.getCourseNumber());
+		values.put(sdc_helper.COLUMN_COURSE_LOCATION, cd.getLocation());
+		values.put(sdc_helper.COLUMN_COURSE_TIME, cd.getTime());
+		
+		long ins_ID = db.insert(SchedumateDatabaseClass.TABLE_COURSE_DETAILS, null,
+		        values);
+		
+		Cursor cursor = db.query(SchedumateDatabaseClass.TABLE_COURSE_DETAILS,
+		        columns_table, SchedumateDatabaseClass.COLUMN_ID + " = " + ins_ID, null,
+		        null, null, null);
+		cursor.moveToFirst();
+		CourseDetails newCourse = cursorToCourseDetails(cursor);
+		cursor.close();
+		
+		return newCourse;
+	}
+	
+	public void deleteCourse(CourseDetails course) {
+		long id = course.getID();
+		System.out.println("Comment deleted with id: " + id);
+		db.delete(SchedumateDatabaseClass.TABLE_COURSE_DETAILS, 
+				SchedumateDatabaseClass.COLUMN_ID
+		    + " = " + id, null);
+	 }
+	
+	public List<CourseDetails> getAllCourses() {
+		List<CourseDetails> courses = new ArrayList<CourseDetails>();
+		
+		Cursor cursor = db.query(SchedumateDatabaseClass.TABLE_COURSE_DETAILS,
+		    columns_table, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			CourseDetails curr_course = cursorToCourseDetails(cursor);
+			courses.add(curr_course);
+			cursor.moveToNext();
+		}
+		// make sure to close the cursor
+		cursor.close();
+		return courses;
+	  }
+	
+	private CourseDetails cursorToCourseDetails(Cursor cursor) {
+	    CourseDetails cd = new CourseDetails();
+	    cd.setID(cursor.getLong(0));
+	    cd.setCourseNumber(cursor.getLong(1));
+		cd.setCourseName(cursor.getString(1));
+		cd.setLocation(cursor.getString(0));
+		cd.setTime(cursor.getString(1));
+	    return cd;
 	}
 }
