2c7a2c8f771f7007f887e28d7c4bf6f18b139142
Aritra Samanta <asamanta@purdue.edu>
2014-10-14 15:53:54 -0400

    EventAdd class created
    EventAdd class helps students add classes/homeworks/events

--- src/com/app/schedumate/EventAdd.java
+++ src/com/app/schedumate/EventAdd.java
@@ -1,5 +1,33 @@
+/*
+ * EventAdd Class - A CLASS THAT HELPS USERS ADDS CLASSES/EVENTS TO
+ * THE DATABSE
+ * 
+ * AUTHOR :- ARITRA SAMANTA
+ * 
+ * CREATED ON - 10/14/2014
+ */
+
 package com.app.schedumate;
 
-public class EventAdd {
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
 
+public class EventAdd {
+	
+	private SQLiteDatabase db;
+	private SchedumateDatabaseClass sdc_helper;
+	private String[] columns_table = {sdc_helper.COLUMN_ID, 
+			sdc_helper.COLUMN_COURSE_NAME,
+			sdc_helper.COLUMN_COURSE_NUMBER,
+			sdc_helper.COLUMN_COURSE_TIME,
+			sdc_helper.COLUMN_COURSE_LOCATION
+		};
+			
+	
+	
+	public EventAdd(Context context)
+	{
+		sdc_helper = new SchedumateDatabaseClass();
+	}
 }
